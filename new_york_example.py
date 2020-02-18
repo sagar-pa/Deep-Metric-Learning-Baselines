c26aa1d9f51be37f9551dba29fe9a4810504eb5a
Sagar <PatelS@tamu.edu>
2020-02-18 17:48:25 -0600

    example visualization

---
+++ new_york_example.py
@@ -0,0 +1,70 @@
+import os, sys, numpy as np, argparse, imp, datetime, time, pickle as pkl, random, json
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+import matplotlib
+matplotlib.use('agg')
+
+from PIL import Image
+import torch, torch.nn as nn
+from torchvision import transforms
+import netlib as netlib
+
+
+import torch.multiprocessing
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+os.environ["CUDA_DEVICE_ORDER"]   ="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]= str(0)
+
+
+"""============================================================================"""
+#################### SEEDS FOR REPROD. #####################
+torch.backends.cudnn.deterministic=True
+np.random.seed(1); random.seed(1)
+torch.manual_seed(1); torch.cuda.manual_seed(1); torch.cuda.manual_seed_all(1)
+
+
+"""============================================================================"""
+##################### NETWORK SETUP ##################
+device = torch.device('cuda')
+#Depending on the choice opt.arch, networkselect() returns the respective network model
+model      = netlib.networkselect('resnet50')
+model.load_state_dict(torch.load('dmt_example.pt'))
+_          = model.to(device)
+
+
+def ensure_3dim(img):
+    if len(img.size) == 2:
+        img = img.convert('RGB')
+    return img
+
+
+data_source = os.getcwd()+ '/Datasets/new_york/'
+data_file = data_source + 'new_york_url_tst_2.txt'
+with open (data_file, 'r') as f:
+    lines = [line.strip() for line in f]
+dataloader = {}
+for line in lines:
+    id, url = line.split()
+    dataloader[id] = data_source+'images/'+id+'.jpg'
+images= {}
+transf_list = []
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+transf_list.extend([transforms.Resize(256),
+                    transforms.CenterCrop(224)])
+transf_list.extend([transforms.ToTensor(), normalize])
+transform = transforms.Compose(transf_list)
+
+for id,loc in dataloader.items():
+    images[id] = transform(ensure_3dim(Image.open(loc)))
+feature_coll = []
+
+model.eval()
+torch.cuda.empty_cache()
+with torch.no_grad():
+    for idx, input in enumerate(images.items()):
+        out = model(input.to(device))
+        feature_coll.extend(out.cpu().detach().numpy().tolist())
+for idx, id in enumerate(images.keys()):
+    repres = data_source + 'dmt_features/' + id + '.npy'
+    np.save(repres, feature_coll[idx])
